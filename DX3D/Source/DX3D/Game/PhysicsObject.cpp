b5a2e1683fd7e8f3a5ea0de2a96eb558c4ed9182
shrimp-n00dle <vingno.elizabeth@gmail.com>
2026-08-07 21:03:02 +0800

    Somewhat

--- DX3D/Source/DX3D/Game/PhysicsObject.cpp
+++ DX3D/Source/DX3D/Game/PhysicsObject.cpp
@@ -1,25 +1,54 @@
-//#include <DX3D/Game/PhysicsObject.h>
-//
-//PhysicsObject::PhysicsObject(const dx3d::GameObjectDesc& desc) : dx3d::GameObject(desc)
-//{
-//
-//}
-//
-//PhysicsObject::~PhysicsObject()
-//{
-//}
-//
-//RigidBody* PhysicsObject::getRigidBody()
-//{
-//	return rigidBody;
-//}
-//
-//void PhysicsObject::onCreate()
-//{
-//	createOrGetComponent<dx3d::MeshComponent>();
-//}
-//
-//void PhysicsObject::onUpdate(dx3d::f32 deltaTime)
-//{
-//	
-//}
+#include <DX3D/Game/PhysicsObject.h>
+
+PhysicsObject::PhysicsObject(const dx3d::GameObjectDesc& desc) : dx3d::GameObject(desc)
+{
+	//Get physicsCommon and World
+	PhysicsCommon* physicsCommon = m_system->getPhysicsCommon();
+	PhysicsWorld* physicsWorld = m_system->getPhysicsWorld();
+
+	//Rigid Body Initialization
+	dx3d::Vec3 scale = this->getTransform().getScale();
+
+	dx3d::Vec3 pos = this->getTransform().getPosition();
+	Vector3 position(pos.x,pos.y,pos.z);
+	Quaternion orientation = rp3d::Quaternion::identity();
+
+	//Transform transform;
+	//transform.setFromOpenGL(this->getTransform()->)
+	Transform transform(position, orientation);
+
+	BoxShape* boxShape = physicsCommon->createBoxShape(Vector3(scale.x/2, scale.y/2, scale.z/2));
+	this->rigidBody = physicsWorld->createRigidBody(transform);
+	this->rigidBody->addCollider(boxShape, transform);
+	this->rigidBody->updateMassPropertiesFromColliders();
+	this->rigidBody->setMass(this->mass);
+	this->rigidBody->setType(BodyType::DYNAMIC);
+
+	transform = this->rigidBody->getTransform();
+	float matrix[16];
+	transform.getOpenGLMatrix(matrix);
+
+	//this->getOwner()->setLocalMatrix(matrix);
+
+
+}
+
+PhysicsObject::~PhysicsObject()
+{
+}
+
+RigidBody* PhysicsObject::getRigidBody()
+{
+	return rigidBody;
+}
+
+void PhysicsObject::onCreate()
+{
+	createOrGetComponent<dx3d::MeshComponent>();
+}
+
+void PhysicsObject::onUpdate(dx3d::f32 deltaTime)
+{
+	const Transform transform = this->rigidBody->getTransform();
+	
+}
--- DX3D/Source/DX3D/Game/PhysicsObject.h
+++ DX3D/Source/DX3D/Game/PhysicsObject.h
@@ -1,27 +1,30 @@
-//#pragma once
-//#include <DX3D/All.h>
-//#include <DX3D/Command/Command.h>
-//#include <DX3D/Math/Vec3.h>
-//
-//#include <reactphysics3d/reactphysics3d.h>
-//
-//using namespace reactphysics3d;
-//
-//class PhysicsObject : public dx3d::GameObject
-//{
-//	dx3d_typeid(PhysicsObject)
-//public:
-//	explicit PhysicsObject(const dx3d::GameObjectDesc& desc);
-//	virtual ~PhysicsObject() override;
-//	dx3d::Vec3 getScale();
-//protected:
-//	virtual void onCreate();
-//	virtual void onUpdate(dx3d::f32 deltaTime);
-//	RigidBody* getRigidBody();
-//
-//
-//public:
-//	float mass = 1000.0f;
-//	RigidBody* rigidBody;
-//};
-//
+#pragma once
+#include <DX3D/All.h>
+
+#include <DX3D/Math/Vec3.h>
+
+#include <reactphysics3d/reactphysics3d.h>
+
+#include <DX3D/Game/PhysicsSystem.h>
+
+using namespace reactphysics3d;
+
+
+class PhysicsObject : public dx3d::GameObject
+{
+	dx3d_typeid(PhysicsObject)
+public:
+	explicit PhysicsObject(const dx3d::GameObjectDesc& desc);
+	virtual ~PhysicsObject() override;
+protected:
+	virtual void onCreate();
+	virtual void onUpdate(dx3d::f32 deltaTime);
+	RigidBody* getRigidBody();
+
+
+public:
+	float mass = 1000.0f;
+	RigidBody* rigidBody;
+	dx3d::RefPtr<dx3d::PhysicsSystem> m_system{};
+};
+
